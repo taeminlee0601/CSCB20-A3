e39dd1842102e4e56861ec6c373c495bfd923f49
nnhoangphong <nnhoangphong@gmail.com>
2024-03-02 23:23:54 -0500

    Paul's Q1 v1

--- Question1.py
+++ Question1.py
@@ -2,24 +2,26 @@ from flask import *
 
 app = Flask(__name__)
 
-@app.route('/<string:username>')
+@app.route('/<string:username>/')
 def welcome(username):
     username = username.strip()
     tmp = ''
     is_low = 0
     is_up = 0
     for i in range(len(username)):
-        if username[i].isalpha():
+        if username[i].isalpha() and not username[i].isdigit():
             tmp += username[i]
             is_low |= username[i].islower()
             is_up |= username[i].isupper()
-    username = tmp
     
-    username = username.lower()
-    if is_low and is_up:
-        username = username[0].upper() + username[1:].lower()
-    elif is_low:
-        username = username.upper()
+    if username == tmp:
+        username = username.lower()
+        if is_low and is_up:
+            username = username[0].upper() + username[1:].lower()
+        elif is_low:
+            username = username.upper()
+    else:
+        username = tmp
     return f'Welcome, {username}, to my CSCB20 Website'
 
 if __name__ == "__main__":
