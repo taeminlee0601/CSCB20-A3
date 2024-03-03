4bfbe7afc8dc2c424f8dac3543484e21848057bf
Bomin75 <dkqnftk313@gmail.com>
2024-03-02 22:54:45 -0500

    Bomin Q1

--- Question1.py
+++ Question1.py
@@ -2,41 +2,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
-<<<<<<< HEAD
-@app.route('/<input>')
-def user(input):
-    # Do not change name or it will change the url
-    name = ''
-
-    for i in range(len(input)):
-        if input[i].isalpha():
-            name += input[i]
-
-    if name != input:
-        return f"Welcome, {name}, to my CSCB20 Website"
-
-    if name.strip() != name:
-        f"Welcome, {name.strip()}, to my CSCB20 Website"
-    
-    if name.isupper():
-        return f"Welcome, {name.lower()}, to my CSCB20 Website"
-    
-    if name.islower():
-        return f"Welcome, {name.upper()}, to my CSCB20 Website"
-
-    name = ''
-
-    for i in range(len(input)):
-        if (i == 0):
-            name += input[i].upper()
-        else:
-            name += input[i].lower()
-
-    return f"Welcome, {name}, to my CSCB20 Website"
-
-if __name__ == '__main__':
-    app.run()
-=======
 @app.route("/")
 def home():
     return "Home"
@@ -65,4 +30,3 @@ def read(id):
 
 if __name__ == '__main__':
     app.run(debug=True)
->>>>>>> ec94992 (Bomin Q1)
