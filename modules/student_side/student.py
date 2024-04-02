302eb6ead41938ed0c4d964eee78fa1d100981cf
Bomin75 <dkqnftk313@gmail.com>
2024-04-01 21:59:02 -0400

    updated

--- modules/student_side/student.py
+++ modules/student_side/student.py
@@ -3,11 +3,13 @@ from flask import Blueprint, render_template
 student = Blueprint("student", __name__)
 
 @student.route('/info')
+@student.route('/student_grades.html')
 def info():
-    
-    return render_template('student_grades.html')
+    pagename = 'info'
+    return render_template('student_grades.html', pagename=pagename)
 
 @student.route('/my_remark_request')
+@student.route('/student_remark.html')
 def my_remark_request():
-
-    return render_template('student_remark.html')
+    pagename = 'my_remark_request'
+    return render_template('student_remark.html', pagename=pagename)
