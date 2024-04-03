8b6506e05fa333a3427a0c85b17a451d2db4f46f
Bomin75 <dkqnftk313@gmail.com>
2024-04-02 22:53:32 -0400

    update

--- static/js/instructor_grades.js
+++ static/js/instructor_grades.js
@@ -2,36 +2,34 @@ $(document).ready(function() {
     $('.ass-manage-grades-btn').click(function() {
         var ass_id = $(this).closest('tr').find('.ass-id').text();
         $.ajax({
-            url: 'http://127.0.0.1:5000/edit_grades',
+            url: 'http://127.0.0.1:5000/instructor_grades.html',
             type: 'POST',
             contentType: 'application/json',
             data: JSON.stringify({ data: ass_id }),
             success: function(response) {
                 console.log('Server response:', response);
+                window.location.href="edit_student_grades.html";
             },
             error: function(error) {
                 console.log('Error:', error);
             }
         });
-
-        window.location.href="edit_student_grades.html";
     });
 
     $('.exam-manage-grades-btn').click(function() {
         var exam_id = $(this).closest('tr').find('.exam-id').text();
         $.ajax({
-            url: 'http://127.0.0.1:5000/edit_grades',
+            url: 'http://127.0.0.1:5000/instructor_grades.html',
             type: 'POST',
             contentType: 'application/json',
             data: JSON.stringify({ data: exam_id }),
             success: function(response) {
                 console.log('Server response:', response);
+                window.location.href="edit_student_grades.html";
             },
             error: function(error) {
                 console.log('Error:', error);
             }
         });
-
-        window.location.href="edit_student_grades.html";
     });
 });
