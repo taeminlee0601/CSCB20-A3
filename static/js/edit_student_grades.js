d298a9fdac0bca9aa34652dafea8ecc43c78c2da
nnhoangphong <nnhoangphong@gmail.com>
2024-04-04 22:05:21 -0400

    fix instructor edit_grade bug

--- static/js/edit_student_grades.js
+++ static/js/edit_student_grades.js
@@ -14,6 +14,7 @@ $(document).ready(function() {
                     data: JSON.stringify({ id: aid, sid: sid, new_grade: new_grade}),
                     success: function(new_url) {
                         console.log('Sent sucessfully');
+                        $('.new-grade').replaceWith('<span class="stu-grade">' + new_grade + '</span>');
                     },
                     error: function(error) {
                         console.log('Error:', error);
